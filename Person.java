8056a3984878483ceee578a7b4e66dbcebda2326
yulong <yulong@rd.tuan800.com>
2013-11-01 19:48:47 +0000

    init

---
+++ Person.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Person
+{
+    // instance variables - replace the example below with your own
+    private int aggressivity;
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person(int aggressivity)
+    {
+        this.aggressivity = aggressivity;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+}
